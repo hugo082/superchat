e3ac5ecc50af38c2581e92fa9cf88cc6452aed27
Hugo Fouquet <hugo.fouquet@epita.fr>
2016-11-11 01:45:39 +0100

    Server transfer to clients

--- server/server.c
+++ server/server.c
@@ -2,168 +2,232 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+
 #include "server.h"
 #include "client.h"
 
-int connected = 0;
-static Client client;
-void server(void)
+static void init(void)
 {
-	SOCKET sock = init_connection();
-	char buffer[BUF_SIZE];
-	int max = sock;
-	fd_set rdfs;
-	while(1)
-	{
-		FD_ZERO(&rdfs);
-		FD_SET(STDIN_FILENO, &rdfs);
-		FD_SET(sock, &rdfs);
-		if(connected)
-		{
-			FD_SET(client.sock, &rdfs);
-		}
-		if(select(max + 1, &rdfs,NULL, NULL, NULL) == -1)
-		{
-			perror("select()");
-			exit(errno);
-		}
-		if(FD_ISSET(STDIN_FILENO, &rdfs))
-		{
-			fgets(buffer, BUF_SIZE - 1, stdin);
-			{
-				char *p = NULL;
-				p = strstr(buffer, "\n");
-				if(p != NULL)
-				{
-					*p = 0;
-				}
-				else
-				{
-					buffer[BUF_SIZE - 1] = 0;
-				}
-			}
-			if(connected && send(client.sock, buffer, strlen(buffer), 0) < 0)
-			{
-				perror("send()");
-				exit(errno);
-			}
-		}
-		else if(FD_ISSET(sock, &rdfs))
-		{
-			SOCKADDR_IN csin = { 0 };
-			size_t sinsize = sizeof csin;
-			int csock = accept(sock, (SOCKADDR *)&csin, &sinsize);
-			if(csock == SOCKET_ERROR)
-			{
-				perror("accept()");
-				continue;
-			}
-			if(read_client(csock, buffer) == -1)
-			{
-				continue;
-			}
-			max = csock > max ? csock : max;
-			FD_SET(csock, &rdfs);
-			Client c = { csock };
-			strncpy(c.name, buffer, BUF_SIZE - 1);
-			client=c;
-			connected=1;
-		}
-		else
-		{
-			if(connected)
-			{
-				if(FD_ISSET(client.sock, &rdfs))
-				{
-					int c = read_client(client.sock, buffer);
-					if(c == 0)
-					{
-						closesocket(client.sock);
-						remove_client(&client);
-						puts("Client déconnecté");
-					}
-					else
-					{
-						printf("%s : ",client.name);
-						puts(buffer);
-					}
-				}
-			}
-		}
-	}
-
-	clear_client(&client);
-	end_connection(sock);
+#ifdef WIN32
+    WSADATA wsa;
+    int err = WSAStartup(MAKEWORD(2, 2), &wsa);
+    if(err < 0)
+    {
+        puts("WSAStartup failed !");
+        exit(EXIT_FAILURE);
+    }
+#endif
 }
 
-static void clear_client(Client *client)
+static void end(void)
 {
-	if(connected)
-	{
-		closesocket(client->sock);
-	}
+#ifdef WIN32
+    WSACleanup();
+#endif
 }
 
-static void remove_client(Client *client)
+static void app(void)
 {
-	memmove(&client,&client + 1, 0);
-	connected--;
+    SOCKET sock = init_connection();
+    char buffer[BUF_SIZE];
+    /* the index for the array */
+    int actual = 0;
+    int max = sock;
+    /* an array for all clients */
+    Client clients[MAX_CLIENTS];
+    
+    fd_set rdfs;
+    
+    printf("Server started.\n");
+    while(1)
+    {
+        int i = 0;
+        FD_ZERO(&rdfs);
+        
+        /* add STDIN_FILENO */
+        FD_SET(STDIN_FILENO, &rdfs);
+        
+        /* add the connection socket */
+        FD_SET(sock, &rdfs);
+        
+        /* add socket of each client */
+        for(i = 0; i < actual; i++)
+        {
+            FD_SET(clients[i].sock, &rdfs);
+        }
+        
+        if(select(max + 1, &rdfs, NULL, NULL, NULL) == -1)
+        {
+            perror("select()");
+            exit(errno);
+        }
+        
+        /* something from standard input : i.e keyboard */
+        if(FD_ISSET(STDIN_FILENO, &rdfs))
+        {
+            /* stop process when type on keyboard */
+            break;
+        }
+        else if(FD_ISSET(sock, &rdfs))
+        {
+            /* new client */
+            SOCKADDR_IN csin = { 0 };
+            size_t sinsize = sizeof csin;
+            int csock = accept(sock, (SOCKADDR *)&csin, &sinsize);
+            if(csock == SOCKET_ERROR)
+            {
+                perror("accept()");
+                continue;
+            }
+            
+            /* after connecting the client sends its name */
+            if(read_client(csock, buffer) == -1)
+            {
+                /* disconnected */
+                continue;
+            }
+            
+            /* what is the new maximum fd ? */
+            max = csock > max ? csock : max;
+            
+            FD_SET(csock, &rdfs);
+            
+            Client c = { csock };
+            strncpy(c.name, buffer, BUF_SIZE - 1);
+            clients[actual] = c;
+            actual++;
+            printf("Client %s connected !\n", c.name);
+        }
+        else
+        {
+            int i = 0;
+            for(i = 0; i < actual; i++)
+            {
+                /* a client is talking */
+                if(FD_ISSET(clients[i].sock, &rdfs))
+                {
+                    Client client = clients[i];
+                    int c = read_client(clients[i].sock, buffer);
+                    /* client disconnected */
+                    if(c == 0)
+                    {
+                        closesocket(clients[i].sock);
+                        remove_client(clients, i, &actual);
+                        strncpy(buffer, client.name, BUF_SIZE - 1);
+                        strncat(buffer, " disconnected !", BUF_SIZE - strlen(buffer) - 1);
+                        send_message_to_all_clients(clients, client, actual, buffer, 1);
+                    }
+                    else
+                    {
+                        printf("%s : %s\n", client.name, buffer);
+                        send_message_to_all_clients(clients, client, actual, buffer, 0);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    
+    clear_clients(clients, actual);
+    end_connection(sock);
 }
 
-static int init_connection(void)
+static void clear_clients(Client *clients, int actual)
 {
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-	SOCKADDR_IN sin = { 0 };
-	if(sock == INVALID_SOCKET)
-	{
-		perror("socket()");
-		exit(errno);
-	}
-	sin.sin_addr.s_addr = htonl(INADDR_ANY);
-	sin.sin_port = htons(PORT);
-	sin.sin_family = AF_INET;
-	if(bind(sock,(SOCKADDR *) &sin, sizeof sin) == SOCKET_ERROR)
-	{
-		perror("bind()");
-		exit(errno);
-	}
-	if(listen(sock,1) == SOCKET_ERROR)
-	{
-		perror("listen()");
-		exit(errno);
-	}
-	return sock;
+    int i = 0;
+    for(i = 0; i < actual; i++)
+    {
+        closesocket(clients[i].sock);
+    }
+    printf("All client have been removed.\n");
 }
 
-static void end_connection(int sock)
-{
-	closesocket(sock);
+static void remove_client(Client *clients, int to_remove, int *actual) {
+    memmove(clients + to_remove, clients + to_remove + 1, (*actual - to_remove - 1) * sizeof(Client));
+    (*actual)--;
 }
 
-static int read_client(SOCKET sock, char *buffer)
-{
-	int n = 0;
-	if((n = recv(sock, buffer, BUF_SIZE - 1, 0)) < 0)
-	{
-		perror("recv()");
-		n = 0;
-	}
-	buffer[n] = 0;
-	return n;
+static void send_message_to_all_clients(Client *clients, Client sender, int actual, const char *buffer, char from_server) {
+    int i = 0;
+    char message[BUF_SIZE];
+    message[0] = 0;
+    for(i = 0; i < actual; i++)
+    {
+        /* we don't send message to the sender */
+        if(sender.sock != clients[i].sock)
+        {
+            if(from_server == 0)
+            {
+                strncpy(message, sender.name, BUF_SIZE - 1);
+                strncat(message, " : ", sizeof message - strlen(message) - 1);
+            }
+            strncat(message, buffer, sizeof message - strlen(message) - 1);
+            write_client(clients[i].sock, message);
+        }
+    }
 }
 
-static void write_client(SOCKET sock, const char *buffer)
-{
-	if(send(sock, buffer, strlen(buffer), 0) < 0)
-	{
-		perror("send()");
-		exit(errno);
-	}
+static int init_connection(void) {
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    SOCKADDR_IN sin = { 0 };
+    
+    if(sock == INVALID_SOCKET)
+    {
+        perror("socket()");
+        exit(errno);
+    }
+    
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_port = htons(PORT);
+    sin.sin_family = AF_INET;
+    
+    //end_connection(sock);
+    
+    if(bind(sock,(SOCKADDR *) &sin, sizeof sin) == SOCKET_ERROR)
+    {
+        perror("bind()");
+        exit(errno);
+    }
+    
+    if(listen(sock, MAX_CLIENTS) == SOCKET_ERROR)
+    {
+        perror("listen()");
+        exit(errno);
+    }
+    printf("Server created.\n");
+    
+    return sock;
+}
+
+static void end_connection(int sock) {
+    closesocket(sock);
+    printf("Server closed.\n");
+}
+
+static int read_client(SOCKET sock, char *buffer) {
+    int n = 0;
+    if((n = recv(sock, buffer, BUF_SIZE - 1, 0)) < 0) {
+        perror("recv()");
+        n = 0;
+    }
+    buffer[n] = 0;
+    return n;
+}
+
+static void write_client(SOCKET sock, const char *buffer) {
+    if(send(sock, buffer, strlen(buffer), 0) < 0) {
+        perror("send()");
+        exit(errno);
+    }
 }
 
 int main(int argc, char **argv)
 {
-	server();
-	return EXIT_SUCCESS;
+    init();
+    
+    app();
+    
+    end();
+    
+    return EXIT_SUCCESS;
 }
-
